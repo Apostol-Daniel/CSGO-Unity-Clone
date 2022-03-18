74cef89aa7c49a6cd59e2ff0b1bd3ce8d39c421a
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-03-18 15:44:42 +0100

    Create Target and ShootingScrtipt

---
+++ src/CSGO Unity Clone/Assets/GunScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    //Don't need start, only update
+    public float damage = 10f;
+    //If object further than 100f then it cannot be hit
+    public float range = 100f;
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Getting player input
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        //Shooting using Ray-Casting(like in csgo, no bullet drop; laser like)
+        void Shoot() 
+        {
+            RaycastHit hitInfo;
+
+            //Check if something is hit
+            if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, range)) 
+            {
+                Debug.Log(hitInfo.transform.name);
+
+                Target target = hitInfo.transform.GetComponent<Target>();
+                if(target != null) 
+                {
+                    target.TakeDamage(damage);
+                }
+            }
+        }
+    }
+}
