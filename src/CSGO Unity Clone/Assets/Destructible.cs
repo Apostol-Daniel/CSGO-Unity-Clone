c28c9811263c9467829b5be26a23f716ff2e2572
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-03-20 13:30:38 +0100

    Make objects destructible

--- src/CSGO Unity Clone/Assets/Destructible.cs
+++ src/CSGO Unity Clone/Assets/Destructible.cs
@@ -1,11 +1,3 @@
-// --------------------------------------
-// This script is totally optional. It is an example of how you can use the
-// destructible versions of the objects as demonstrated in my tutorial.
-// Watch the tutorial over at http://youtube.com/brackeys/.
-// --------------------------------------
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Destructible : MonoBehaviour {
