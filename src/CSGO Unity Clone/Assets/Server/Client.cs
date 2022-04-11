50edeaded236afeac925589e99362367f2d4ec7b
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-11 14:01:01 +0200

    Create Client and Tcp on unity client side

---
+++ src/CSGO Unity Clone/Assets/Server/Client.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Assets.Server
+{
+    public class Client : MonoBehaviour
+    {
+        public static Client Instance;
+        public static int DataBufferSize = 4096;
+
+        public string Ip = "127.0.0.1";
+
+        public int Port = 26950;
+        public int ClientId = 0;
+        public Tcp TcpClient;
+
+        private void Awake()
+        {
+            if(Instance == null) 
+            {
+                Instance = this;
+            }
+
+            else if (Instance != this) 
+            {
+                Debug.Log("Instance already exists, detroying object.");
+                Destroy(this);
+            }
+        }
+
+        private void Start()
+        {
+            TcpClient = new Tcp(Instance, DataBufferSize);
+        }
+
+        public void ConnectToServer()
+        {
+            
+        }
+    }
+}
---
+++ src/CSGO Unity Clone/Assets/Server/Tcp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Server
+{
+    public class Tcp
+    {
+        public TcpClient Socket;
+
+        private NetworkStream Stream;
+        private byte[] ReceiveBuffer;
+        private readonly int DataBufferSize;
+        private readonly Client Instance;
+
+        public Tcp(Client instance,int dataBufferSize)
+        {
+            DataBufferSize = dataBufferSize;
+            Instance = instance;
+        }
+
+        public void Connect(TcpClient socket) 
+        {
+            Socket = socket;
+
+            Socket.ReceiveBufferSize = DataBufferSize;
+            Socket.SendBufferSize = DataBufferSize;
+
+            ReceiveBuffer = new byte[DataBufferSize];
+            Socket.BeginConnect(Instance.Ip, Instance.Port, ConnectCallback, Socket);
+        }
+
+        private void ConnectCallback(IAsyncResult result) 
+        {
+            Socket.EndConnect(result);
+
+            if (!Socket.Connected)
+            {
+                return;
+            }
+
+            Stream = Socket.GetStream();
+
+            Stream.BeginRead(ReceiveBuffer, 0, DataBufferSize, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult result)
+        {
+            try
+            {
+                int byteLenght = Stream.EndRead(result);
+                if (byteLenght <= 0)
+                {
+                    return;
+                }
+
+                byte[] data = new byte[byteLenght];
+                Array.Copy(ReceiveBuffer, data, byteLenght);
+
+                Stream.BeginRead(ReceiveBuffer, 0, DataBufferSize, ReceiveCallback, null);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error receiving TCP data : {ex}");
+            }
+        }
+    }
+
+
+}
