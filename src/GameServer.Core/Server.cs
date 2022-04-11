5aaee80588a4bef68bb541320b371fc1442c5e6d
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-11 12:32:27 +0200

    Create GameServer with basic scaffloding

---
+++ src/GameServer.Core/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer.Core
+{
+    public class Server
+    {
+        public static int MaxPlayers { get; private set; }
+
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+        public static void Start(int mapPlayes, int portNumber) 
+        {
+            MaxPlayers = mapPlayes;
+            Port = portNumber;
+
+            Console.WriteLine("Starting server...");
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TcpConnectCallback), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+        }
+
+        private static void TcpConnectCallback(IAsyncResult result) 
+        {
+            TcpClient tcpClient = tcpListener.EndAcceptTcpClient(result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TcpConnectCallback), null);
+        }
+    }
+}
---
+++ src/GameServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
