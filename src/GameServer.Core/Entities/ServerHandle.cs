ee4b4f9a5a26cf15ae31938dd639044c04b09b17
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-12 11:47:15 +0200

    Create ServerHandle in GameServer

---
+++ src/GameServer.Core/Entities/ServerHandle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer.Core.Entities
+{
+    public class ServerHandle
+    {
+        public static void WelcomeReeived(int clientId, Packet packet) 
+        {
+            int clientIdControl = packet.ReadInt();
+            string userName = packet.ReadString();
+
+            Console.WriteLine($"{Server.Clients[clientId].Tcp.Socket.Client.RemoteEndPoint} connected successfully and is now player {userName}.");
+            if(clientId != clientIdControl) 
+            {
+                //This message should never get printed; if it is, something went horribly wrong
+                Console.WriteLine($"Playerr \"{userName}\" (Id:{clientId} has assumed the wrong client Id ({clientIdControl})!");
+            }
+        }
+    }
+}
