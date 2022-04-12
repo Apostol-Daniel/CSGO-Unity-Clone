f445ab1ef8f88567a6828a393c2f10dc9527ecd7
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-12 13:51:37 +0200

    Handle sending udp data in Server (GameServer)

--- src/GameServer.Core/Entities/Server.cs
+++ src/GameServer.Core/Entities/Server.cs
@@ -60,6 +60,66 @@ namespace GameServer.Core
             Console.WriteLine($"{tcpClient.Client.RemoteEndPoint} failed to connect: Server is full.");
         }
 
+        private static void UdpReceiveCallback(IAsyncResult result) 
+        {
+            try
+            {
+                IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[]  data = UdpListener.EndReceive(result, ref clientEndPoint);
+                UdpListener.BeginReceive(UdpReceiveCallback, null);
+
+                if(data.Length < 4) 
+                {
+                    return;
+                }
+
+                using(Packet packet = new Packet(data)) 
+                {
+                    int clientId = packet.ReadInt();
+
+                    //Safety check; if clientId == 0 then the server crashes
+                    if(clientId == 0) 
+                    {
+                        return;
+                    }
+
+                    //If UdpEndPoint == null => new connection from new client;package is empty => Connect to server and then not handle the data
+                    if(Clients[clientId].Udp.UdpEndPoint == null) 
+                    {
+                        Clients[clientId].Udp.Connect(clientEndPoint);
+                        return;
+                    }
+
+                    //Check if endpoints match; hacker could send another enpoint
+                    //ToString() because just comparing Endpoint to Endpoint will return false
+                    if(Clients[clientId].Udp.UdpEndPoint.ToString() == clientEndPoint.ToString()) 
+                    {
+                        //Finally handle data
+                        Clients[clientId].Udp.HandleData(packet);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error receiving UDP data: {ex}.");
+            }
+        }
+
+        public static void SendUdpData(IPEndPoint clientEndPoint, Packet packet) 
+        {
+            try
+            {
+                if (clientEndPoint != null) 
+                {
+                    UdpListener.BeginSend(packet.ToArray(), packet.Length(), clientEndPoint, null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending UDP data: {ex} to {clientEndPoint}.");
+            }
+        }
+
         private static void InitializeServerData() 
         {
             for (int i = 1; i <= MaxPlayers; i++)
