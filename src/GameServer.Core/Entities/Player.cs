1ca822f6d3bb5ee35f583973a8a2a060e8e049db
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-12 17:40:53 +0200

    Create Player class for GameServer

---
+++ src/GameServer.Core/Entities/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace GameServer.Core.Entities
+{
+    public class Player
+    {
+        public int Id;
+        public string UserName;
+
+        public Vector3 Position;
+
+        public Quaternion Rotation;
+
+        public Player(int id, string userName, Vector3 spawnPosition)
+        {
+            Id = id;
+            UserName = userName;
+            Position = spawnPosition;
+            Rotation = Quaternion.Identity;
+        }
+    }
+}
