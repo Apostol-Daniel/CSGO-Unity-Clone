b51e29eaf1eacde17a3000d8daaa108bc4e95381
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-13 11:11:25 +0200

    Update Scenes assets and prefabs

---
+++ src/CSGO Unity Clone/Assets/Server/GameManager.cs
@@ -0,0 +1,44 @@
+using Assets.Server;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public static Dictionary<int, PlayerManager> Players = new Dictionary<int, PlayerManager>();
+
+    public GameObject LocalPlayerPrefab;
+    public GameObject PlayerPrefab;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else if (Instance != this)
+        {
+            Debug.Log("Instance already exists, detroying object.");
+            Destroy(this);
+        }
+    }
+
+    public void SpawnPlayer(int playerId, string userName, Vector3 position, Quaternion rotation) 
+    {
+        GameObject player;
+        if (playerId == Client.ClientInstance.ClientId)
+        {
+            player = Instantiate(LocalPlayerPrefab, position, rotation);
+
+        }
+        else 
+        {
+            player = Instantiate(PlayerPrefab, position, rotation);
+        }
+
+        player.GetComponent<PlayerManager>().Id = playerId;
+        player.GetComponent<PlayerManager>().UserName = userName;
+        Players.Add(playerId, player.GetComponent<PlayerManager>());
+    }
+}
---
+++ src/CSGO Unity Clone/Assets/Server/PlayerManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int Id;
+    public string UserName;
+}
