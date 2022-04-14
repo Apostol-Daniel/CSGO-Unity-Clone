fcd832c4b0119ee7e0af891694d59d1abcdfde20
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-14 11:07:53 +0200

    Add Space KeyCode to PlayerController in Client

--- src/CSGO Unity Clone/Assets/Server/PlayerController.cs
+++ src/CSGO Unity Clone/Assets/Server/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
             Input.GetKey(KeyCode.S),
             Input.GetKey(KeyCode.A),
             Input.GetKey(KeyCode.D),
+            Input.GetKey(KeyCode.Space),
         };
 
         ClientSend.PlayerMovement(inputs);
