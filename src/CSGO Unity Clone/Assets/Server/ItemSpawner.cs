2553da28d075bda6c83a9657b2fe630cd7804f45
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-15 14:43:56 +0200

    Make item spawner rotate and bob

--- src/CSGO Unity Clone/Assets/Server/ItemSpawner.cs
+++ src/CSGO Unity Clone/Assets/Server/ItemSpawner.cs
@@ -8,7 +8,18 @@ public class ItemSpawner : MonoBehaviour
     public bool HasItem;
     public MeshRenderer ItemModel;
 
+    public float ItemRotationSpeed = 50f;
+    public float ItemBobSpeed = 2f;
     private Vector3 BasePosition;
+
+    private void Update()
+    {
+        if (HasItem) 
+        {
+            transform.Rotate(Vector3.up, ItemRotationSpeed * Time.deltaTime, Space.World);
+            transform.position = BasePosition + new Vector3(0f, 0.25f * Mathf.Sin(Time.time * ItemBobSpeed), 0f);
+        }
+    }
 
     public void Initialize(int spawnerId, bool hasItem)
     {
