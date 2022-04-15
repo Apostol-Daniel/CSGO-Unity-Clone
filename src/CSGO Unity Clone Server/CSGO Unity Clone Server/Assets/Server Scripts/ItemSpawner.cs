67ede4dee30e6d8b351b55902b83dccb1eee1c8f
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-04-15 13:35:25 +0200

    Create ItemSpawner in Server

---
+++ src/CSGO Unity Clone Server/CSGO Unity Clone Server/Assets/Server Scripts/ItemSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public static Dictionary<int, ItemSpawner> Spawners = new Dictionary<int, ItemSpawner>();
+
+    private static int NextSpwanerId = 1;
+
+    public int SpawnerId;
+    public bool HasItem = false;
+
+    private void Start()
+    {
+        HasItem = false;
+        SpawnerId = NextSpwanerId;
+        NextSpwanerId++;
+        Spawners.Add(SpawnerId, this);
+
+        StartCoroutine(SpawnItem());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Player player = other.GetComponentInParent<Player>();
+            if (player.AttemptPickUpItem()) 
+            {
+                ItemPickUp(player.Id);
+            }
+        }
+    }
+
+    private IEnumerator SpawnItem()
+    {
+        yield return new WaitForSeconds(10f);
+
+        HasItem = true;
+    }
+
+    private void ItemPickUp(int playerId) 
+    {
+        HasItem = false;
+
+        StartCoroutine(SpawnItem());
+    }
+
+}
+
