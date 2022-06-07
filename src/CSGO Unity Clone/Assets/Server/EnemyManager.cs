23d4e2070810fe1c97ca8803cab0c9ee56aefeae
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-06-07 02:03:00 +0200

    Remove Enemy from scene from EnemyManager

--- src/CSGO Unity Clone/Assets/Server/EnemyManager.cs
+++ src/CSGO Unity Clone/Assets/Server/EnemyManager.cs
@@ -22,6 +22,7 @@ namespace Assets.Server
 
             if(Health <= 0f) 
             {
+                GameManager.Enemies.Remove(Id);
                 Destroy(gameObject);
             }
         }
