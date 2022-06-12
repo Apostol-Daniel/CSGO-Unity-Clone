97b7ca0bb54853e3a013056088fc1b1f8d361e98
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2022-06-12 05:37:34 +0200

    Add Health an Grenades UI elements for multiplayer

--- src/CSGO Unity Clone/Assets/Server/PlayerManager.cs
+++ src/CSGO Unity Clone/Assets/Server/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -10,17 +11,25 @@ public class PlayerManager : MonoBehaviour
     public float MaxHealth;
     public int ItemCount = 0;
     public MeshRenderer Model;
+    public Text TxtHealth;
+    public Text TxtGrenades;
+
+    public void FixedUpdate()
+    {
+        TxtGrenades.text = ItemCount.ToString();
+        TxtHealth.text = Health.ToString();
+    }
 
     public void Initialize(int id, string userName)
     {
         Id = id;
         UserName = userName;
-        Health = MaxHealth;
+        Health = MaxHealth;    
     }
 
     public void SetHealth(float health) 
     {
-        Health = health;
+        Health = health;     
 
         if(health <= 0f) 
         {
